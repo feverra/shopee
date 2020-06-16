ce6cd819ea582008754984b72fc8678502028bac
unknown <feverra@gmail.com>
2020-06-16 21:10:22 +0700

    16

--- Shopee-Farm.user.js
+++ Shopee-Farm.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Shopee Farm Every 2 Hours
 // @namespace    http://tampermonkey.net/
-// @version      0.1.2
+// @version      0.1.4
 // @description  try to take over the world!
 // @author       You
 // @match        https://games.shopee.co.th/
@@ -9,40 +9,69 @@
 // @grant        none
 // ==/UserScript==
 
-(() => {
+(()=> {
     'use strict';
-    document.title = 'farm';
-    var jqry = document.createElement('script');
-    jqry.src = "https://dmama.pwa.co.th/mis/demo/Javascript/Bootstrap/jquery-3.2.1.min.js";
-    document.getElementsByTagName('head')[0].appendChild(jqry);
-
-    setInterval(() => {
-        $.get("https://games.shopee.co.th/farm/api/friend/share_link/get", data => {
-            console.log(data)
-        });
-
-        $.get("https://games.shopee.co.th/farm/api/orchard/context/get", data => {
-            console.log(data);
-            var cropId = data.data.crops[0].id
-            var resourceId = data.data.resources[0].id
-            var water = data.data.resources[0].number
-            var json = JSON.stringify({
-                "cropId": cropId,
-                "resourceId": resourceId
-            })
-            console.log('json', json);
-            console.log('water', water);
-            if (water > 10) {
-                $.ajax({
-                    type: "POST",
-                    url: "https://games.shopee.co.th/farm/api/orchard/crop/water",
-                    data: json,
-                    contentType: "application/json; charset=utf-8",
-                    dataType: "json",
-                    success: data => { console.log(data); },
-                    failure: errMsg => { console.log(errMsg); }
-                });
-            }
-        })
+    document.title = 'farm'
+    //window.str = ''
+    var str = ""
+    var jqry = document.createElement('script')
+    jqry.src = "https://code.jquery.com/jquery-3.5.1.min.js"
+    document.getElementsByTagName('head')[0].appendChild(jqry)
+    setInterval(()=>{
+        farm()
     }, 7200000);
+
+    setTimeout(()=>{
+        $('body').append('<textarea id="txt" style="width: 100%;height: 100%"></textarea>');
+        console.log('ตั้งเวลา')
+        echo('ตั้งเวลา')
+        farm()
+    },3000)
+    setTimeout(()=>{
+        location.reload()
+    }, 28800000)
 })();
+function echo(val = ''){
+    //window.str += "\n" + val
+    //$('#txt').val(window.str)
+    document.getElementById("txt").value += "\n" + val
+
+}
+function farm(){
+    /*$.get( "https://games.shopee.co.th/farm/api/friend/share_link/get", data => {
+            console.log(data)
+        })*/
+
+    $.get( "https://games.shopee.co.th/farm/api/orchard/context/get", data => {
+        //console.log(data);
+        var cropId = data.data.crops[0].id
+        var resourceId = data.data.resources[0].id
+        var water = data.data.resources[0].number
+        var json = JSON.stringify({
+            "cropId": cropId,
+            "resourceId": resourceId
+        })
+        console.log('json', json);
+        console.log('water', water);
+        echo('มีน้ำเหลือ : ' + water);
+        if(water > 1){
+            $.ajax({
+                type: "POST",
+                url: "https://games.shopee.co.th/farm/api/orchard/crop/water",
+                data: json,
+                contentType: "application/json; charset=utf-8",
+                dataType: "json",
+                success: data => {
+                    console.log(data);
+                    console.log('รดน้ำ');
+                    echo('รดน้ำ');
+                },
+                failure: errMsg => {
+                    console.log(errMsg);
+                    echo('รดไม่ได้อะ');
+                }
+            });
+        }
+
+    })
+}
