2fba572e29c261ab528bcc5e8e5386ac807ba1f9
unknown <feverra@gmail.com>
2020-07-22 16:06:10 +0700

    farm

--- Shopee-Farm.user.js
+++ Shopee-Farm.user.js
@@ -1,11 +1,11 @@
-// ==UserScript==
+﻿// ==UserScript==
 // @name         Shopee Farm Every 2 Hours
 // @namespace    http://tampermonkey.net/
 // @version      0.1.5
 // @description  try to take over the world!
 // @author       You
 // @match        https://games.shopee.co.th/
-// @downloadURL  https://github.com/feverra/shopee/raw/master/Shopee-Farm.user.js
+// downloadURL  https://github.com/feverra/shopee/raw/master/Shopee-Farm.user.js
 // @grant        none
 // ==/UserScript==
 
@@ -24,22 +24,20 @@
         console.log('ตั้งเวลา')
         echo('ตั้งเวลา')
         farm()
-    }, 1000)
+    }, 400)
     setTimeout(() => {
         location.reload()
     }, 28800000)
 })()
 function echo(val = '') {
     document.getElementById("txt").value += "\n" + val
-
 }
 function farm() {
     /*$.get( "https://games.shopee.co.th/farm/api/friend/share_link/get", data => {
             console.log(data)
         })*/
-
-    $.get("https://games.shopee.co.th/farm/api/orchard/context/get", data => {
-        //console.log(data)
+    $.get("https://games.shopee.co.th/farm/api/orchard/context/get").done(data => {
+        console.log(data)
         var cropId = data.data.crops[0].id
         var resourceId = data.data.resources[0].id
         var water = data.data.resources[0].number
@@ -68,6 +66,41 @@ function farm() {
                 }
             })
         }
-
+    }).fail(() => {
+        echo("ไปล็อกอินด้วย");
     })
+
+    // $.get("https://games.shopee.co.th/farm/api/orchard/context/get", data => {
+    //console.log(data)
+    // var cropId = data.data.crops[0].id
+    // var resourceId = data.data.resources[0].id
+    // var water = data.data.resources[0].number
+    // var json = JSON.stringify({
+    //     "cropId": cropId,
+    //     "resourceId": resourceId
+    // })
+    // console.log('json', json)
+    // console.log('water', water)
+    // echo('มีน้ำเหลือ : ' + water)
+    // if (water > 0) {
+    //     $.ajax({
+    //         type: "POST",
+    //         url: "https://games.shopee.co.th/farm/api/orchard/crop/water",
+    //         data: json,
+    //         contentType: "application/json charset=utf-8",
+    //         dataType: "json",
+    //         success: data => {
+    //             console.log(data)
+    //             console.log('รดน้ำ')
+    //             echo('รดน้ำ')
+    //         },
+    //         failure: errMsg => {
+    //             console.log(errMsg)
+    //             echo('รดไม่ได้อะ')
+    //         }
+    //     })
+    // }
+    // }).fail(function () {
+    //     echo("error");
+    // })
 }
