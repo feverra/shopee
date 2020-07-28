a11e7657608ed31f1ca27e859f71c88724afd04a
unknown <feverra@gmail.com>
2020-07-28 23:09:27 +0700

    add time

--- Shopee-Farm.user.js
+++ Shopee-Farm.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Shopee Farm Every 2 Hours
 // @namespace    http://tampermonkey.net/
-// @version      0.1.6
+// @version      0.1.7
 // @description  try to take over the world!
 // @author       You
 // @match        https://games.shopee.co.th/
@@ -15,9 +15,10 @@
     var jqry = document.createElement('script')
     jqry.src = "https://code.jquery.com/jquery-3.5.1.min.js"
     document.getElementsByTagName('head')[0].appendChild(jqry)
+    let timeLoop = getRandomInt(100000) + 2000000
     setInterval(() => {
         farm()
-    }, 1800000)
+    }, timeLoop)
 
     setTimeout(() => {
         $('body').append('<textarea id="txt" style="width: 100%;height: 100%;"></textarea>')
@@ -71,38 +72,8 @@ function farm() {
     }).fail(() => {
         echo("ไปล็อกอินด้วย");
     })
+}
 
-    // $.get("https://games.shopee.co.th/farm/api/orchard/context/get", data => {
-    //console.log(data)
-    // var cropId = data.data.crops[0].id
-    // var resourceId = data.data.resources[0].id
-    // var water = data.data.resources[0].number
-    // var json = JSON.stringify({
-    //     "cropId": cropId,
-    //     "resourceId": resourceId
-    // })
-    // console.log('json', json)
-    // console.log('water', water)
-    // echo('มีน้ำเหลือ : ' + water)
-    // if (water > 0) {
-    //     $.ajax({
-    //         type: "POST",
-    //         url: "https://games.shopee.co.th/farm/api/orchard/crop/water",
-    //         data: json,
-    //         contentType: "application/json charset=utf-8",
-    //         dataType: "json",
-    //         success: data => {
-    //             console.log(data)
-    //             console.log('รดน้ำ')
-    //             echo('รดน้ำ')
-    //         },
-    //         failure: errMsg => {
-    //             console.log(errMsg)
-    //             echo('รดไม่ได้อะ')
-    //         }
-    //     })
-    // }
-    // }).fail(function () {
-    //     echo("error");
-    // })
+function getRandomInt(max = 10) {
+    return Math.floor(Math.random() * Math.floor(max));
 }
